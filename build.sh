83589fb73dc6309173a02c060c2e4130e8deaf3c
shrir <shriraj1223shetty@gmail.com>
2026-04-11 09:30:12 +0530

    Apply .gitignore and remove ignored files from tracking

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 echo "Building the project..."
-
+pip install setuptools
+pip install -r requirements.txt
 # Use python3 generically
 export PATH=$PATH:/usr/local/bin
 
 echo "Collect Static..."
 python3 manage.py collectstatic --noinput --clear
+python3 manage.py makemigrations
+python3 manage.py migrate
+
 
 echo "Build complete."
