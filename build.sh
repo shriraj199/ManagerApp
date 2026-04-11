7b71843a5135e6bd5ba2854257868aeb135fe4e4
shrir <shriraj1223shetty@gmail.com>
2026-04-11 10:08:15 +0530

    Update build script with pip install and migrations

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 echo "Building the project..."
+
 pip install setuptools
 pip install -r requirements.txt
-# Use python3 generically
-export PATH=$PATH:/usr/local/bin
 
 echo "Collect Static..."
 python3 manage.py collectstatic --noinput --clear
-python3 manage.py makemigrations
-python3 manage.py migrate
 
+echo "Running migrations..."
+python3 manage.py migrate
 
 echo "Build complete."
