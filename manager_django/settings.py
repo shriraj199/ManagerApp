245e1c3e90b1cfbec3497c48d61821858a0a3aad
shrir <shriraj1223shetty@gmail.com>
2026-04-11 09:41:48 +0530

    Update ALLOWED_HOSTS for Vercel and remove unused dj_database_url import

--- manager_django/settings.py
+++ manager_django/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from decouple import config
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','*']
 
 
 # Application definition
